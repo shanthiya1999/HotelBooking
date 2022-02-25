b9ed9baacb96114d55b9d486396e0eb55929c5c1
A Shanthiya <ashanthiya1999@gmail.com>
2022-02-25 13:26:01 +0530

    Update LoginUser.java

--- src/main/java/com/hcl/LoginUser.java
+++ src/main/java/com/hcl/LoginUser.java
@@ -12,19 +12,28 @@ public class LoginUser {
 	@Column(name="email")
 	private String email;
 	@Column(name="password")
-	private int password;
+	private String password;
+	@Column (name = "Username")
+private String username;
+
+public String getUsername() {
+	return username;
+}
+public void setUsername(String username) {
+	this.username = username;
+}
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public int getPassword() {
-		return password;
-	}
-	public void setPassword(int password) {
-		this.password = password;
-	}
+	public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
 	
 
 }
